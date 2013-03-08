8205a97c7e31cae4f31fc8537ff9488ae086742f
はく <haku@vaguehope.com>
2013-03-08 20:54:20 +0000

    Use Handler to refresh UI on correct thread.

--- src/com/vaguehope/onosendai/ui/TweetListFragment.java
+++ src/com/vaguehope/onosendai/ui/TweetListFragment.java
@@ -1,8 +1,11 @@
 package com.vaguehope.onosendai.ui;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +27,14 @@ public class TweetListFragment extends Fragment {
 	private int columnId;
 	private TweetListAdapter adapter;
 	private DbClient bndDb;
+	private RefreshUiHandler refreshUiHandler;
 
 	@Override
 	public View onCreateView (final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
 		this.columnId = getArguments().getInt(ARG_COLUMN_ID);
 		this.log.setPrefix("C" + this.columnId);
 
+		this.refreshUiHandler = new RefreshUiHandler(this);
 		ListView tweetList = new ListView(getActivity());
 		this.adapter = new TweetListAdapter(getActivity());
 		tweetList.setAdapter(this.adapter);
@@ -61,11 +66,10 @@ public class TweetListFragment extends Fragment {
 				@Override
 				public void run () {
 					/*
-					 * this convoluted method is because the service
-					 * connection won't finish until this thread processes
-					 * messages again (i.e., after it exits this thread). if
-					 * we try to talk to the DB service before then, it will
-					 * NPE.
+					 * this convoluted method is because the service connection
+					 * won't finish until this thread processes messages again
+					 * (i.e., after it exits this thread). if we try to talk to
+					 * the DB service before then, it will NPE.
 					 */
 					getBndDb().getDb().addTwUpdateListener(getGuiUpdateRunnable());
 					refreshUi();
@@ -108,6 +112,26 @@ public class TweetListFragment extends Fragment {
 	};
 
 	protected void refreshUi () {
+		this.refreshUiHandler.sendEmptyMessage(0);
+	}
+
+	private static class RefreshUiHandler extends Handler {
+
+		private final WeakReference<TweetListFragment> parentRef;
+
+		public RefreshUiHandler (final TweetListFragment parent) {
+			this.parentRef = new WeakReference<TweetListFragment>(parent);
+		}
+
+		@Override
+		public void handleMessage (final Message msg) {
+			TweetListFragment parent = this.parentRef.get();
+			if (parent != null) parent.refreshUiOnUiThread();
+		}
+
+	}
+
+	protected void refreshUiOnUiThread () {
 		ArrayList<Tweet> tweets = this.bndDb.getDb().getTweets(this.columnId, 200);
 		this.adapter.setInputData(new TweetList(tweets));
 		this.log.i("Refreshed %d tweets.", tweets.size());
