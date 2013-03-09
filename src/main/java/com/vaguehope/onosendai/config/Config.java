fe304dac69fe479e62dd98d1c50db093d8db4166
はく <haku@vaguehope.com>
2013-03-09 13:16:12 +0000

    improve sample config.  config pkg.

--- src/main/java/com/vaguehope/onosendai/config/Config.java
+++ src/main/java/com/vaguehope/onosendai/config/Config.java
@@ -1,13 +1,15 @@
-package com.vaguehope.onosendai;
+package com.vaguehope.onosendai.config;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import android.os.Environment;
 
+import com.vaguehope.onosendai.C;
 import com.vaguehope.onosendai.util.FileHelper;
 
 public class Config {
@@ -15,7 +17,7 @@ public class Config {
 	public Config () throws IOException, JSONException {
 		File f = new File(Environment.getExternalStorageDirectory().getPath(), C.CONFIG_FILE_NAME);
 		String s = FileHelper.fileToString(f);
-		Object o = new JSONTokener(s).nextValue();
+		JSONObject o = (JSONObject) new JSONTokener(s).nextValue();
 		System.err.println(o);
 	}
 
