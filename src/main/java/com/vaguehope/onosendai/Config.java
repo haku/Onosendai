9044a1c86a0831f62e0fdb4638fe4c8be61d7c4d
はく <haku@vaguehope.com>
2013-03-09 12:16:13 +0000

    basic passing test

--- src/main/java/com/vaguehope/onosendai/Config.java
+++ src/main/java/com/vaguehope/onosendai/Config.java
@@ -6,12 +6,14 @@ import java.io.IOException;
 import org.json.JSONException;
 import org.json.JSONTokener;
 
+import android.os.Environment;
+
 import com.vaguehope.onosendai.util.FileHelper;
 
 public class Config {
 
 	public Config () throws IOException, JSONException {
-		File f = C.CONFIG_FILE;
+		File f = new File(Environment.getExternalStorageDirectory().getPath(), C.CONFIG_FILE_NAME);
 		String s = FileHelper.fileToString(f);
 		Object o = new JSONTokener(s).nextValue();
 		System.err.println(o);
