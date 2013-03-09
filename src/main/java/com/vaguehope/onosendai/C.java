a3a619f3ff1edd7081e70b256098a0bf72951717
はく <haku@vaguehope.com>
2013-03-09 12:16:13 +0000

    basic passing test

--- src/main/java/com/vaguehope/onosendai/C.java
+++ src/main/java/com/vaguehope/onosendai/C.java
@@ -1,14 +1,11 @@
 package com.vaguehope.onosendai;
 
-import java.io.File;
-
-import android.os.Environment;
 
 public interface C {
 
 	String TAG = "onosendai";
 
-	File CONFIG_FILE = new File(Environment.getExternalStorageDirectory().getPath(), "deck.conf");
+	String CONFIG_FILE_NAME = "deck.conf";
 
 	String DATA_TW_MAX_AGE_DAYS = "-7 days";
 
--- src/test/java/com/vaguehope/onosendai/ConfigTest.java
+++ src/test/java/com/vaguehope/onosendai/ConfigTest.java
@@ -1,39 +1,45 @@
 package com.vaguehope.onosendai;
 
-import java.io.BufferedReader;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import android.os.Environment;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ Environment.class })
 public class ConfigTest {
 
-	private Config undertest;
+	@Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
 	@Before
 	public void before () throws Exception {
-		this.undertest = new Config();
+		PowerMockito.mockStatic(Environment.class);
+		when(Environment.getExternalStorageDirectory()).thenReturn(this.temporaryFolder.getRoot());
+		String conf = fixture("/deck.conf");
+		File f = new File(this.temporaryFolder.getRoot(), "deck.conf");
+		FileUtils.write(f, conf);
 	}
 
 	@Test
-	public void itDoesSomething () throws Exception {
-		System.err.println("desu");
+	public void itReadsConfig () throws Exception {
+		Config conf = new Config();
 	}
 
 	private String fixture (final String path) throws IOException {
-		StringBuilder s = new StringBuilder();
-		BufferedReader r = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(path)));
-		try {
-			String line;
-			while ((line = r.readLine()) != null) {
-				s.append(line).append("\n");
-			}
-			return s.toString();
-		}
-		finally {
-			r.close();
-		}
+		return IOUtils.toString(getClass().getResourceAsStream(path));
 	}
 
 }
