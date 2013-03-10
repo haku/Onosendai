60de0908fc81095218bdb6b606d9a07e82a6b27d
はく <haku@vaguehope.com>
2013-03-10 14:07:06 +0000

    save list position based on item ID.

--- src/main/java/com/vaguehope/onosendai/util/ListViewHelper.java
+++ src/main/java/com/vaguehope/onosendai/util/ListViewHelper.java
@@ -1,6 +1,7 @@
 package com.vaguehope.onosendai.util;
 
 import android.view.View;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public final class ListViewHelper {
@@ -9,35 +10,46 @@ public final class ListViewHelper {
 		throw new AssertionError();
 	}
 
+	public static ScrollState saveScrollState (final ListView lv) {
+		int index = lv.getFirstVisiblePosition();
+		View v = lv.getChildAt(0);
+		int top = (v == null) ? 0 : v.getTop();
+
+		long itemId = lv.getAdapter().getItemId(index);
+
+		return new ScrollState(itemId, top);
+	}
+
+	public static void restoreScrollState (final ListView lv, final ScrollState state) {
+		if (state == null) return;
+		ListAdapter adapter = lv.getAdapter();
+		for (int i = 0; i < adapter.getCount(); i++) {
+			if (adapter.getItemId(i) == state.itemId) {
+				lv.setSelectionFromTop(i, state.top);
+				return;
+			}
+		}
+	}
+
 	public static class ScrollState {
 
-		final int index;
+		final long itemId;
 		final int top;
 
-		public ScrollState (final int index, final int top) {
-			this.index = index;
+		public ScrollState (final long itemId, final int top) {
+			this.itemId = itemId;
 			this.top = top;
 		}
 
 		@Override
 		public String toString () {
 			return new StringBuilder()
-					.append("SaveScrollState{").append(this.index).append(',').append(this.top).append('}')
+					.append("SaveScrollState{").append(this.itemId)
+					.append(',').append(this.top)
+					.append('}')
 					.toString();
 		}
 
-	}
-
-	public static ScrollState saveScrollState (final ListView lv) {
-		int index = lv.getFirstVisiblePosition();
-		View v = lv.getChildAt(0);
-		int top = (v == null) ? 0 : v.getTop();
-		return new ScrollState(index, top);
-	}
-
-	public static void restoreScrollState (final ListView lv, final ScrollState state) {
-		if (state == null) return;
-		lv.setSelectionFromTop(state.index, state.top);
 	}
 
 }
