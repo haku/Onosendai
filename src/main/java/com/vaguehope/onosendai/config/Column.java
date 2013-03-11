b3bf2f9c21727cff1bfc48eed93b4abb59097dfb
はく <haku@vaguehope.com>
2013-03-11 22:53:37 +0000

    Fetch columns using multiple threads if needed.

--- src/main/java/com/vaguehope/onosendai/config/Column.java
+++ src/main/java/com/vaguehope/onosendai/config/Column.java
@@ -1,5 +1,7 @@
 package com.vaguehope.onosendai.config;
 
+import com.vaguehope.onosendai.util.EqualHelper;
+
 public class Column {
 
 	public final int id;
@@ -14,6 +16,31 @@ public class Column {
 		this.accountId = accountId;
 		this.resource = resource;
 		this.refresh = refresh;
+	}
+
+	@Override
+	public int hashCode () {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.accountId == null) ? 0 : this.accountId.hashCode());
+		result = prime * result + this.id;
+		result = prime * result + ((this.refresh == null) ? 0 : this.refresh.hashCode());
+		result = prime * result + ((this.resource == null) ? 0 : this.resource.hashCode());
+		result = prime * result + ((this.title == null) ? 0 : this.title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals (final Object o) {
+		if (o == null) return false;
+		if (o == this) return true;
+		if (!(o instanceof Column)) return false;
+		Column that = (Column) o;
+		return EqualHelper.equal(this.id, that.id) &&
+				EqualHelper.equal(this.title, that.title) &&
+				EqualHelper.equal(this.accountId, that.accountId) &&
+				EqualHelper.equal(this.resource, that.resource) &&
+				EqualHelper.equal(this.refresh, that.refresh);
 	}
 
 	@Override
