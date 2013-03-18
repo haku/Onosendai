198e4b222c263a224550f9eddfca3e0b7b8ca9a1
はく <haku@vaguehope.com>
2013-03-18 21:45:43 +0000

    First pass at link extraction.

--- src/main/java/com/vaguehope/onosendai/payload/PayloadUtils.java
+++ src/main/java/com/vaguehope/onosendai/payload/PayloadUtils.java
@@ -2,11 +2,15 @@ package com.vaguehope.onosendai.payload;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.vaguehope.onosendai.model.Tweet;
 
-
 public final class PayloadUtils {
+
+	private static final String URL_REGEX = "\\(?\\b(https?://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]";
+	private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
 	private PayloadUtils () {
 		throw new AssertionError();
@@ -14,8 +18,17 @@ public final class PayloadUtils {
 
 	public static PayloadList extractPayload (final Tweet tweet) {
 		List<Payload> ret = new ArrayList<Payload>();
-
+		extractUrls(tweet.getBody(), ret);
 		return new PayloadList(ret);
+	}
+
+	private static void extractUrls (final String text, final List<Payload> result) {
+		Matcher m = URL_PATTERN.matcher(text);
+		while (m.find()) {
+			String g = m.group();
+			if (g.startsWith("(") && g.endsWith(")")) g = g.substring(1, g.length() - 1);
+			result.add(new LinkPayload(g));
+		}
 	}
 
 }
---
+++ src/test/java/com/vaguehope/onosendai/payload/PayloadUtilsTest.java
@@ -0,0 +1,70 @@
+package com.vaguehope.onosendai.payload;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.vaguehope.onosendai.model.Tweet;
+
+public class PayloadUtilsTest {
+
+	@Test
+	public void itParsesALinkFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link http://example.com/foo123.", "http://example.com/foo123");
+	}
+
+	@Test
+	public void itParsesMultipleLinksFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link http://example.com/foo123.  And another www.example.com/bar123...",
+				"http://example.com/foo123", "www.example.com/bar123");
+	}
+
+	@Test
+	public void itParsesAnHttpsLinkFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link https://example.com/foo123.", "https://example.com/foo123");
+	}
+
+	@Test
+	public void itParsesALinkInBacketsFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link (http://example.com/foo123).", "http://example.com/foo123");
+	}
+
+	@Test
+	public void itParsesALinkInSquareBacketsFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link [http://example.com/foo123].", "http://example.com/foo123");
+	}
+
+	@Test
+	public void itParsesALinkInQuotesFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link \"http://example.com/foo123\".", "http://example.com/foo123");
+	}
+
+	@Test
+	public void itParsesALinkWithoutSchemeButWwwFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link www.example.com/foo123.", "www.example.com/foo123");
+	}
+
+	@Test
+	public void itParsesALinkWithAFragmentFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link http://example.com/foo123#abc.", "http://example.com/foo123#abc");
+	}
+
+	@Ignore("Not sure how to support this one.")
+	@Test
+	public void itParsesALinkWithoutSchemeOrWwwFromTweet () throws Exception {
+		textLinkExtraction("A tweet with a link example.com/foo123.", "example.com/foo123");
+	}
+
+	private static void textLinkExtraction (final String body, final String... expectedUrls) {
+		Tweet tweet = new Tweet(0L, "", body, 0L);
+		PayloadList payloadList = PayloadUtils.extractPayload(tweet);
+		assertEquals(expectedUrls.length, payloadList.size());
+		for (int i = 0; i < expectedUrls.length; i++) {
+			Payload payload = payloadList.getPayload(i);
+			assertEquals(PayloadType.LINK, payload.getType());
+			assertEquals(expectedUrls[i], ((LinkPayload) payload).getUrl());
+		}
+	}
+
+}
