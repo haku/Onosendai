4c3ff2039c25cfb4588357a6aaa1a264f8ff3fa7
はく <haku@vaguehope.com>
2013-03-23 15:53:54 +0000

    Simplify PayloadListClickListener and fix potential stale ref issue.

--- src/main/java/com/vaguehope/onosendai/payload/PayloadListClickListener.java
+++ src/main/java/com/vaguehope/onosendai/payload/PayloadListClickListener.java
@@ -1,31 +1,25 @@
 package com.vaguehope.onosendai.payload;
 
-import android.content.Context;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class PayloadListClickListener implements OnItemClickListener {
 
-	private final Context context;
-	private final PayloadListAdapter payloadAdaptor;
-
-	public PayloadListClickListener (final Context context, final PayloadListAdapter payloadAdaptor) {
-		this.context = context;
-		this.payloadAdaptor = payloadAdaptor;
-	}
+	public PayloadListClickListener () {}
 
 	@Override
 	public void onItemClick (final AdapterView<?> parent, final View view, final int position, final long id) {
-		Payload payload = this.payloadAdaptor.getInputData().getPayload(position);
+		final ListView lv = (ListView) parent;
+		final Payload payload = (Payload) lv.getAdapter().getItem(position);
 		if (payload.intentable()) {
-			this.context.startActivity(payload.toIntent());
+			parent.getContext().startActivity(payload.toIntent());
 		}
 		else {
-			Toast.makeText(this.context, "Do not know how to show: " + payload.getTitle(), Toast.LENGTH_LONG).show();
+			Toast.makeText(parent.getContext(), "Do not know how to show: " + payload.getTitle(), Toast.LENGTH_LONG).show();
 		}
 	}
-
 
 }
