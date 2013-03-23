31cae7bdc5c5ffbb542c9d9b66336b3b74ffd5c3
はく <haku@vaguehope.com>
2013-03-23 00:48:54 +0000

    If image is already in memory, load in main thread and do not fork.

--- src/main/java/com/vaguehope/onosendai/images/ImageLoaderUtils.java
+++ src/main/java/com/vaguehope/onosendai/images/ImageLoaderUtils.java
@@ -1,6 +1,9 @@
 package com.vaguehope.onosendai.images;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+
+import com.vaguehope.onosendai.R;
 
 public final class ImageLoaderUtils {
 
@@ -8,9 +11,20 @@ public final class ImageLoaderUtils {
 		throw new AssertionError();
 	}
 
-	public static ImageLoader fromActivity(final Activity activity) {
+	public static ImageLoader fromActivity (final Activity activity) {
 		if (!(activity instanceof ImageLoader)) throw new IllegalArgumentException("Not an ImageLoader: " + activity);
 		return (ImageLoader) activity;
+	}
+
+	public static void loadImage (final BitmapCache<String> cache, final ImageLoadRequest req) {
+		final Bitmap bmp = cache.get(req.getUrl());
+		if (bmp != null) {
+			req.getImageView().setImageBitmap(bmp);
+		}
+		else {
+			req.getImageView().setImageResource(R.drawable.question_blue);
+			new ImageFetcherTask(cache).execute(req);
+		}
 	}
 
 }
