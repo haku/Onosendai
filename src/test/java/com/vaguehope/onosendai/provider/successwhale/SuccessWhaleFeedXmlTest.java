6447eae2bdf1ce0e4b5b7cb87b6fe255c6828735
はく <haku@vaguehope.com>
2013-03-27 09:59:52 +0000

    Update SW XML parser to match new format.

--- src/test/java/com/vaguehope/onosendai/provider/successwhale/SuccessWhaleFeedXmlTest.java
+++ src/test/java/com/vaguehope/onosendai/provider/successwhale/SuccessWhaleFeedXmlTest.java
@@ -12,24 +12,41 @@ import com.vaguehope.onosendai.model.TweetList;
 public class SuccessWhaleFeedXmlTest {
 
 	@Test
-	public void itParsesTweets () throws Exception {
+	public void itParsesASimpleTweets () throws Exception {
 		SuccessWhaleFeedXml feed = new SuccessWhaleFeedXml(getClass().getResourceAsStream("/successwhale_tweets.xml"));
 		TweetList tweets = feed.getTweets();
 		assertEquals(2, tweets.count());
 
-		Tweet t0 = tweets.getTweet(0);
-		assertEquals("first tweets message", t0.getBody());
-		assertEquals("313781345934852800", t0.getSid());
-		assertEquals("some_user_name", t0.getUsername());
-		assertEquals(1363646281L, t0.getTime());
-		assertEquals("http://si0.twimg.com/profile_images/2344890405/af462a16c498087e4372cb6ac6aff134_normal.jpeg", t0.getAvatarUrl());
+		Tweet t = tweets.getTweet(0);
+		assertEquals("first tweets message", t.getBody());
+		assertEquals("313781345934852800", t.getSid());
+		assertEquals("some_user_name", t.getUsername());
+		assertEquals(1363646281L, t.getTime());
+		assertEquals("http://si0.twimg.com/profile_images/2344890405/af462a16c498087e4372cb6ac6aff134_normal.jpeg", t.getAvatarUrl());
+	}
 
-		Tweet t1 = tweets.getTweet(1);
-		assertEquals("The second tweets message.", t1.getBody());
-		assertEquals("313434534579697409", t1.getSid());
-		assertEquals("some_other_user", t1.getUsername());
-		assertEquals(1363563060L, t1.getTime());
-		assertEquals("http://si0.twimg.com/profile_images/19393922/informal_profile_small2_normal.jpg", t1.getAvatarUrl());
+	@Test
+	public void itPareseATweetWithLinkAndPreview () throws Exception {
+		SuccessWhaleFeedXml feed = new SuccessWhaleFeedXml(getClass().getResourceAsStream("/successwhale_tweets.xml"));
+		TweetList tweets = feed.getTweets();
+		assertEquals(2, tweets.count());
+
+		Tweet t = tweets.getTweet(1);
+		assertEquals("DreamWorks tops compute-cycle record with 'The Croods' http://t.co/zG68KbAFDX", t.getBody());
+		assertEquals("339045390394853920", t.getSid());
+		assertEquals("some_other_user", t.getUsername());
+		assertEquals(1364374692L, t.getTime());
+		assertEquals("http://si0.twimg.com/profile_images/0983453383/e987609785a770b7ceeb476525435a1f_normal.jpeg", t.getAvatarUrl());
+
+		assertEquals(2, t.getMetas().size());
+
+		Meta m0 = t.getMetas().get(0);
+		assertEquals(MetaType.MEDIA, m0.getType());
+		assertEquals("http://pbs.twimg.com/media/SDFLKsdflkjdfmG.jpg", m0.getData());
+
+		Meta m1 = t.getMetas().get(1);
+		assertEquals(MetaType.URL, m1.getType());
+		assertEquals("http://www.computerworld.com/s/article/9237880/DreamWorks_tops_compute_cycle_record_with_The_Croods_", m1.getData());
 	}
 
 	@Test
@@ -38,17 +55,22 @@ public class SuccessWhaleFeedXmlTest {
 		TweetList tweets = feed.getTweets();
 		assertEquals(3, tweets.count());
 
-		Tweet t0 = tweets.getTweet(0);
-		assertEquals("Some User shared a link.", t0.getBody());
-		assertEquals("557897893_497789789789963", t0.getSid());
-		assertEquals(null, t0.getUsername());
-		assertEquals("Some User", t0.getFullname());
-		assertEquals(1364295194L, t0.getTime());
+		Tweet t = tweets.getTweet(0);
+		assertEquals("Some User shared a link.", t.getBody());
+		assertEquals("557897893_497789789789963", t.getSid());
+		assertEquals(null, t.getUsername());
+		assertEquals("Some User", t.getFullname());
+		assertEquals(1364295194L, t.getTime());
 
-		assertEquals(1, t0.getMetas().size());
-		Meta t0m0 = t0.getMetas().get(0);
-		assertEquals(MetaType.URL, t0m0.getType());
-		assertEquals("http://9gag.com/gag/6904527?ref=fb.s", t0m0.getData());
+		assertEquals(2, t.getMetas().size());
+
+		Meta m0 = t.getMetas().get(0);
+		assertEquals(MetaType.MEDIA, m0.getType());
+		assertEquals("https://s-platform.ak.fbcdn.net/www/app_full_proxy.php?app=350685531728&v=1&size=z&cksum=1e87fdc3550bd207da99c7d631a82ae0&src=http%3A%2F%2Fnews.bbcimg.co.uk%2Fmedia%2Fimages%2F66629000%2Fjpg%2F_66629196_66629188.jpg", m0.getData());
+
+		Meta m1 = t.getMetas().get(1);
+		assertEquals(MetaType.URL, m1.getType());
+		assertEquals("http://m.bbc.co.uk/news/world-asia-21950139", m1.getData());
 	}
 
 	// TODO conversation entry.
