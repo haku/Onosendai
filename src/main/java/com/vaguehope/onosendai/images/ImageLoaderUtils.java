addcbbb48dba51155891c464dec1c5220c9b7a7d
はく <haku@vaguehope.com>
2013-03-27 13:20:12 +0000

    Fix image load listener not always called.

--- src/main/java/com/vaguehope/onosendai/images/ImageLoaderUtils.java
+++ src/main/java/com/vaguehope/onosendai/images/ImageLoaderUtils.java
@@ -3,8 +3,6 @@ package com.vaguehope.onosendai.images;
 import android.app.Activity;
 import android.graphics.Bitmap;
 
-import com.vaguehope.onosendai.R;
-
 public final class ImageLoaderUtils {
 
 	private ImageLoaderUtils () {
@@ -19,12 +17,11 @@ public final class ImageLoaderUtils {
 	public static void loadImage (final HybridBitmapCache cache, final ImageLoadRequest req) {
 		final Bitmap bmp = cache.quickGet(req.getUrl());
 		if (bmp != null) {
-			req.getImageView().setImageBitmap(bmp);
+			req.setImageBitmap(bmp);
 		}
 		else {
-			req.getImageView().setImageResource(R.drawable.question_blue);
-			// TODO if this becomes multi-threaded, need to lock in each unique URL
-			// to avoid duplicate downloads.
+			req.setImagePending();
+			// TODO if this becomes multi-threaded, need to lock in each unique URL to avoid duplicate downloads.
 			new ImageFetcherTask(cache).execute(req);
 		}
 	}
