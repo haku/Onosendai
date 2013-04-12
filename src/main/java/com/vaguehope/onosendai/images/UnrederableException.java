7fe9967c12d0dee21880978579e048021a2bb251
はく <haku@vaguehope.com>
2013-04-12 22:47:47 +0100

    Separate images that do not render from images that are not in the cache.  Do not re-fetch images that do not render.

---
+++ src/main/java/com/vaguehope/onosendai/images/UnrederableException.java
@@ -0,0 +1,14 @@
+package com.vaguehope.onosendai.images;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UnrederableException extends IOException {
+
+	private static final long serialVersionUID = -5115494476279391567L;
+
+	public UnrederableException (final File file) {
+		super("Unrederable: " + file.getAbsolutePath());
+	}
+
+}
