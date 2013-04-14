ef6b3b0a03a153e12ff94195659cfa2a294687ac
はく <haku@vaguehope.com>
2013-04-14 11:02:36 +0100

    Merge FileHelper into IoHelper.

--- src/main/java/com/vaguehope/onosendai/util/IoHelper.java
+++ src/main/java/com/vaguehope/onosendai/util/IoHelper.java
@@ -1,8 +1,15 @@
 package com.vaguehope.onosendai.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 public final class IoHelper {
 
@@ -21,6 +28,47 @@ public final class IoHelper {
 			bytesReadTotal += bytesRead;
 		}
 		return bytesReadTotal;
+	}
+
+	/**
+	 * Returns null if file does not exist.
+	 */
+	public static String fileToString (final File file) throws IOException {
+		try {
+			final FileInputStream stream = new FileInputStream(file);
+			try {
+				final FileChannel fc = stream.getChannel();
+				final MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+				/* Instead of using default, pass in a decoder. */
+				return Charset.defaultCharset().decode(bb).toString();
+			}
+			finally {
+				stream.close();
+			}
+		}
+		catch (final FileNotFoundException e) {
+			return null;
+		}
+	}
+
+	public static void resourceToFile(final String res, final File f) throws IOException {
+		final byte[] arr = new byte[COPY_BUFFER_SIZE];
+		final InputStream is = IoHelper.class.getResourceAsStream(res);
+		try {
+			final OutputStream os = new FileOutputStream(f);
+			try {
+				int count;
+				while ((count = is.read(arr)) >= 0) {
+					os.write(arr, 0, count);
+				}
+			}
+			finally {
+				os.close();
+			}
+		}
+		finally {
+			is.close();
+		}
 	}
 
 }
