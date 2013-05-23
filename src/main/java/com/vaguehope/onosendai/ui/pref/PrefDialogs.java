d3acb7ccbc723f01cfdd6084bf0ca61d7b01eaf8
はく <haku@vaguehope.com>
2013-05-23 20:15:09 +0100

    Prompt for account when adding a new column.

--- src/main/java/com/vaguehope/onosendai/ui/pref/PrefDialogs.java
+++ src/main/java/com/vaguehope/onosendai/ui/pref/PrefDialogs.java
@@ -5,16 +5,24 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.json.JSONException;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 
+import com.vaguehope.onosendai.config.Account;
 import com.vaguehope.onosendai.config.AccountProvider;
+import com.vaguehope.onosendai.config.Prefs;
 import com.vaguehope.onosendai.util.CollectionHelper;
 import com.vaguehope.onosendai.util.CollectionHelper.Function;
 import com.vaguehope.onosendai.util.DialogHelper;
 
 public final class PrefDialogs {
+
+	public interface Listener<T> {
+		public void onAnswer (T answer);
+	}
 
 	protected static final List<AccountProvider> NEW_ACCOUNT_PROVIDERS = Collections.unmodifiableList(Arrays.asList(new AccountProvider[] {
 			AccountProvider.TWITTER,
@@ -33,7 +41,7 @@ public final class PrefDialogs {
 		throw new AssertionError();
 	}
 
-	public static void askAccountType (final Context context, final Listener<AccountProvider> onAccount) {
+	public static void askAccountType (final Context context, final Listener<AccountProvider> onAccountType) {
 		final AlertDialog.Builder bld = new AlertDialog.Builder(context);
 		bld.setTitle("Account Type");
 		bld.setNegativeButton("Cancel", DialogHelper.DLG_CANCEL_CLICK_LISTENER);
@@ -41,14 +49,45 @@ public final class PrefDialogs {
 			@Override
 			public void onClick (final DialogInterface dialog, final int item) {
 				dialog.dismiss();
-				onAccount.onAnswer(NEW_ACCOUNT_PROVIDERS.get(item));
+				onAccountType.onAnswer(NEW_ACCOUNT_PROVIDERS.get(item));
 			}
 		});
 		bld.show();
 	}
 
-	public interface Listener<T> {
-		public void onAnswer (T answer);
+	public static void askAccount (final Context context, final Prefs prefs, final Listener<Account> onAccount) {
+		final AlertDialog.Builder bld = new AlertDialog.Builder(context);
+		bld.setTitle("Account");
+		bld.setNegativeButton("Cancel", DialogHelper.DLG_CANCEL_CLICK_LISTENER);
+
+		final List<Account> accounts = readAccountsOrAlert(context, prefs);
+		if (accounts == null) return;
+
+		final List<String> labels = CollectionHelper.map(accounts, new Function<Account, String>() {
+			@Override
+			public String exec (final Account account) {
+				return account.toHumanString();
+			}
+		}, new ArrayList<String>());
+
+		bld.setItems(labels.toArray(new String[] {}), new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick (final DialogInterface dialog, final int item) {
+				dialog.dismiss();
+				onAccount.onAnswer(accounts.get(item));
+			}
+		});
+		bld.show();
+	}
+
+	private static List<Account> readAccountsOrAlert (final Context context, final Prefs prefs) {
+		try {
+			return new ArrayList<Account>(prefs.readAccounts());
+		}
+		catch (JSONException e) {
+			DialogHelper.alert(context, "Failed to read accounts.", e);
+			return null;
+		}
 	}
 
 }
