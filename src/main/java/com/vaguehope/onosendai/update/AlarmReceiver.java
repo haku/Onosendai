953f7b895db1a6a7c736c1cb9efb1e1af48722a1
はく <haku@vaguehope.com>
2013-08-03 16:47:07 +0100

    Once-per-day cleanup service that deletes old scaled pictures.

--- src/main/java/com/vaguehope/onosendai/update/AlarmReceiver.java
+++ src/main/java/com/vaguehope/onosendai/update/AlarmReceiver.java
@@ -11,38 +11,69 @@ import android.os.SystemClock;
 import android.util.Log;
 
 import com.vaguehope.onosendai.C;
+import com.vaguehope.onosendai.util.LogWrapper;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
 	private static final int TEMP_WAKELOCK_TIMEOUT_MILLIS = 3000;
 
+	private static final int BASE_ALARM_ID = 11000;
+	private static final String KEY_ACTION = "action";
+	private static final int ACTION_UPDATE = 1;
+	private static final int ACTION_CLEANUP = 2;
+
+	private static final LogWrapper LOG = new LogWrapper("AR");
+
 	@Override
 	public void onReceive (final Context context, final Intent intent) {
-		Log.i(C.TAG, "AlarmReceiver invoked.");
+		final int action = intent.getExtras().getInt(KEY_ACTION, -1);
+		LOG.i("AlarmReceiver invoked: action=%s.", action);
 
-		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-		WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, C.TAG);
-		wl.acquire(TEMP_WAKELOCK_TIMEOUT_MILLIS);
-
-		context.startService(new Intent(context, UpdateService.class));
+		switch (action) {
+			case ACTION_UPDATE:
+				final PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+				final WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, C.TAG);
+				wl.acquire(TEMP_WAKELOCK_TIMEOUT_MILLIS);
+				context.startService(new Intent(context, UpdateService.class));
+				break;
+			case ACTION_CLEANUP:
+				context.startService(new Intent(context, CleanupService.class));
+				break;
+			default:
+				LOG.e("Unknown action: '%s'.", action);
+				break;
+		}
 	}
 
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	public static void configureAlarms (final Context context) {
+		final AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		scheduleUpdates(context, am);
+		scheduleCleanups(context, am);
+		Log.i(C.TAG, "Alarm service configured.");
+	}
 
-	public static void configureAlarm (final Context context) {
-		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		PendingIntent i = getPendingIntent(context);
+	private static void scheduleUpdates (final Context context, final AlarmManager am) {
+		final PendingIntent i = PendingIntent.getBroadcast(context, BASE_ALARM_ID + ACTION_UPDATE,
+				new Intent(context, AlarmReceiver.class).putExtra(KEY_ACTION, ACTION_UPDATE),
+				PendingIntent.FLAG_CANCEL_CURRENT);
 		am.cancel(i);
 		am.setInexactRepeating(
 				AlarmManager.ELAPSED_REALTIME_WAKEUP,
 				SystemClock.elapsedRealtime(),
 				AlarmManager.INTERVAL_FIFTEEN_MINUTES,
 				i);
-		Log.i(C.TAG, "Alarm service configured.");
 	}
 
-	private static PendingIntent getPendingIntent (final Context context) {
-		return PendingIntent.getBroadcast(context, 0, new Intent(context, AlarmReceiver.class), PendingIntent.FLAG_CANCEL_CURRENT);
+	private static void scheduleCleanups (final Context context, final AlarmManager am) {
+		final PendingIntent i = PendingIntent.getBroadcast(context, BASE_ALARM_ID + ACTION_CLEANUP,
+				new Intent(context, AlarmReceiver.class).putExtra(KEY_ACTION, ACTION_CLEANUP),
+				PendingIntent.FLAG_CANCEL_CURRENT);
+		am.cancel(i);
+		am.setInexactRepeating(
+				AlarmManager.ELAPSED_REALTIME,
+				SystemClock.elapsedRealtime(),
+				AlarmManager.INTERVAL_DAY,
+				i);
 	}
 
 }
