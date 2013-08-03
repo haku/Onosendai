eb762683395b16c3ff09a1260e181368d91feded
はく <haku@vaguehope.com>
2013-08-03 13:32:46 +0100

    Picture shrink dlg shows preview.

--- src/main/java/com/vaguehope/onosendai/ui/PictureResizeDialog.java
+++ src/main/java/com/vaguehope/onosendai/ui/PictureResizeDialog.java
@@ -8,19 +8,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import local.apache.ByteArrayOutputStream;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.vaguehope.onosendai.R;
-import com.vaguehope.onosendai.util.CountingDevNull;
 import com.vaguehope.onosendai.util.ImageMetadata;
 import com.vaguehope.onosendai.util.IoHelper;
 import com.vaguehope.onosendai.util.Titleable;
@@ -30,15 +34,17 @@ public class PictureResizeDialog implements Titleable {
 	private static final List<Scale> SCALES = Scale.setOf(100, 75, 50, 25);
 	private static final int DEFAULT_SCALES_POS = 0;
 
-	private static final List<Scale> QUALITIES = Scale.setOf(100, 95, 90, 80, 70, 50, 30);
+	private static final List<Scale> QUALITIES = Scale.setOf(100, 95, 90, 80, 70, 50, 30, 10);
 	private static final int DEFAULT_QUALITY_POS = 2;
 
 	private final Context context;
 	private final ImageMetadata srcMetadata;
 
 	private final View llParent;
+	private final ImageView imgPreview;
 	private final Spinner spnScale;
 	private final Spinner spnQuality;
+	private final ProgressBar prgRedrawing;
 	private final TextView txtSummary;
 
 	public PictureResizeDialog (final Context context, final Uri pictureUri) throws IOException {
@@ -51,8 +57,10 @@ public class PictureResizeDialog implements Titleable {
 		final LayoutInflater inflater = LayoutInflater.from(context);
 		this.llParent = inflater.inflate(R.layout.pictureresizedialog, null);
 
+		this.imgPreview = (ImageView) this.llParent.findViewById(R.id.imgPreview);
 		this.spnScale = (Spinner) this.llParent.findViewById(R.id.spnScale);
 		this.spnQuality = (Spinner) this.llParent.findViewById(R.id.spnQuality);
+		this.prgRedrawing = (ProgressBar) this.llParent.findViewById(R.id.prgRedrawing);
 		this.txtSummary = (TextView) this.llParent.findViewById(R.id.txtSummary);
 
 		final ArrayAdapter<Scale> scaleAdapter = new ArrayAdapter<Scale>(context, R.layout.numberspinneritem);
@@ -117,31 +125,7 @@ public class PictureResizeDialog implements Titleable {
 	};
 
 	protected void updateSummary () {
-		final StringBuilder s = new StringBuilder();
-		try {
-			final Bitmap src = this.srcMetadata.readBitmap();
-			s.append(src.getWidth()).append(" x ").append(src.getHeight())
-					.append(" (").append(IoHelper.readableFileSize(this.srcMetadata.getSize())).append(")");
-
-			final int w = scaleDimension(src.getWidth());
-			final int h = scaleDimension(src.getHeight());
-			s.append(" --> ").append(w).append(" x ").append(h);
-
-			final Bitmap shrunk = Bitmap.createScaledBitmap(src, w, h, true);
-			try {
-				final CountingDevNull cdn = new CountingDevNull();
-				shrunk.compress(Bitmap.CompressFormat.JPEG, getQuality().getPercentage(), cdn);
-				s.append(" (").append(IoHelper.readableFileSize(cdn.getCount())).append(")");
-			}
-			finally {
-				if (shrunk != src) shrunk.recycle();
-			}
-		}
-		catch (final IOException e) {
-			s.delete(0, s.length());
-			s.append(e.toString());
-		}
-		this.txtSummary.setText(s.toString());
+		new PreviewResizeTask(this.txtSummary, this.imgPreview, this.prgRedrawing, this.srcMetadata, this).execute();
 	}
 
 	private File getTempOutputFile () {
@@ -151,7 +135,7 @@ public class PictureResizeDialog implements Titleable {
 		return new File(baseDir, name);
 	}
 
-	private int scaleDimension (final int from) {
+	protected int scaleDimension (final int from) {
 		final Scale s = getScale();
 		if (s.getPercentage() == 100) return from;
 		return (int) (from * s.getPercentage() / 100d);
@@ -182,6 +166,73 @@ public class PictureResizeDialog implements Titleable {
 				ret.add(new Scale(i, i + "%"));
 			}
 			return Collections.unmodifiableList(ret);
+		}
+
+	}
+
+	private static class PreviewResizeTask extends AsyncTask<Void, Void, Bitmap> {
+
+		private final TextView txtSummary;
+		private final ImageView imgPreview;
+		private final ProgressBar prgRedrawing;
+		private final StringBuilder summary;
+		private final ImageMetadata srcMetadata;
+		private final PictureResizeDialog dlg;
+
+		public PreviewResizeTask (final TextView txtSummary, final ImageView imgPreview, final ProgressBar prgRedrawing, final ImageMetadata srcMetadata, final PictureResizeDialog dlg) {
+			this.txtSummary = txtSummary;
+			this.imgPreview = imgPreview;
+			this.prgRedrawing = prgRedrawing;
+			this.srcMetadata = srcMetadata;
+			this.dlg = dlg;
+			this.summary = new StringBuilder();
+		}
+
+		@Override
+		protected void onPreExecute () {
+			this.prgRedrawing.setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		protected Bitmap doInBackground (final Void... params) {
+			try {
+				final Bitmap src = this.srcMetadata.readBitmap();
+				final int w = this.dlg.scaleDimension(src.getWidth());
+				final int h = this.dlg.scaleDimension(src.getHeight());
+				final Bitmap scaled = Bitmap.createScaledBitmap(src, w, h, true);
+				try {
+					final ByteArrayOutputStream compOut = new ByteArrayOutputStream(512 * 1024);
+					if (scaled.compress(Bitmap.CompressFormat.JPEG, this.dlg.getQuality().getPercentage(), compOut)) {
+						this.summary
+								.append(src.getWidth()).append(" x ").append(src.getHeight())
+								.append(" (").append(IoHelper.readableFileSize(this.srcMetadata.getSize())).append(")")
+								.append(" --> ").append(w).append(" x ").append(h)
+								.append(" (").append(IoHelper.readableFileSize(compOut.size())).append(")");
+						return BitmapFactory.decodeStream(compOut.toBufferedInputStream());
+					}
+					this.summary.append("Failed to compress image.");
+					return null;
+				}
+				finally {
+					if (scaled != src) scaled.recycle();
+				}
+			}
+			catch (final IOException e) {
+				this.summary.append(e.toString());
+				return null;
+			}
+		}
+
+		@Override
+		protected void onPostExecute (final Bitmap result) {
+			this.txtSummary.setText(this.summary.toString());
+			if (result != null) {
+				this.imgPreview.setImageBitmap(result);
+			}
+			else {
+				this.imgPreview.setImageResource(R.drawable.exclamation_red);
+			}
+			this.prgRedrawing.setVisibility(View.INVISIBLE);
 		}
 
 	}
