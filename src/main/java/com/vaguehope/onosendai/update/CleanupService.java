a18e7471a265be873e04ef2a9e35344ea13bb7bd
はく <haku@vaguehope.com>
2013-08-15 07:37:02 +0100

    Only do DB cleanup in manually triggered housekeep.

--- src/main/java/com/vaguehope/onosendai/update/CleanupService.java
+++ src/main/java/com/vaguehope/onosendai/update/CleanupService.java
@@ -1,35 +1,38 @@
 package com.vaguehope.onosendai.update;
 
+import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 
 import com.vaguehope.onosendai.images.HybridBitmapCache;
 import com.vaguehope.onosendai.storage.AttachmentStorage;
-import com.vaguehope.onosendai.storage.DbBindingService;
 import com.vaguehope.onosendai.util.LogWrapper;
 
 /*
  * TODO move this class?
  */
-public class CleanupService extends DbBindingService {
+public class CleanupService extends IntentService {
 
 	protected static final LogWrapper LOG = new LogWrapper("CS");
 
 	public CleanupService () {
-		super("OnosendaiCleanupService", LOG);
+		super("OnosendaiCleanupService");
 	}
 
 	@Override
-	protected void doWork (final Intent i) {
+	protected final void onHandleIntent (final Intent i) {
 		try {
-			AttachmentStorage.cleanTempOutputDir(this); // FIXME what if attachment in use in Outbox?
-			HybridBitmapCache.cleanCacheDir(this);
-			if (!waitForDbReady()) return;
-			getDb().housekeep();
+			clean(this);
 			LOG.i("Clean up complete.");
 		}
 		catch (final Exception e) { // NOSONAR want to log all errors.
 			LOG.e("Clean up failed.", e);
 		}
+	}
+
+	public static void clean (final Context context) {
+		AttachmentStorage.cleanTempOutputDir(context); // FIXME what if attachment in use in Outbox?
+		HybridBitmapCache.cleanCacheDir(context);
 	}
 
 }
