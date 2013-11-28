1a629a8dbb4e9cfca77426a7e0feb8a72ae2c4ec
はく <haku@vaguehope.com>
2013-11-28 20:42:27 +0000

    Gracefully handle "Status is a duplicate" response. [issue #52]

--- src/main/java/com/vaguehope/onosendai/provider/TaskUtils.java
+++ src/main/java/com/vaguehope/onosendai/provider/TaskUtils.java
@@ -2,10 +2,13 @@ package com.vaguehope.onosendai.provider;
 
 import twitter4j.TwitterException;
 
+import com.vaguehope.onosendai.model.TaskOutcome;
 import com.vaguehope.onosendai.provider.successwhale.SuccessWhaleException;
 import com.vaguehope.onosendai.util.StringHelper;
 
 public final class TaskUtils {
+
+	private static final int TWITTER_ERROR_CODE_STATUS_IS_A_DUPLICATE = 187;
 
 	private TaskUtils () {
 		throw new AssertionError();
@@ -21,21 +24,22 @@ public final class TaskUtils {
 		return StringHelper.isEmpty(e.getMessage()) ? e.toString() : e.getMessage();
 	}
 
-	public static boolean isFailurePermanent(final Exception e) {
+	public static TaskOutcome failureType(final Exception e) {
 		if (e instanceof TwitterException) {
 			final TwitterException te = (TwitterException) e;
-			if (te.isCausedByNetworkIssue()) return false;
-			if (te.exceededRateLimitation()) return false;
+			if (te.isCausedByNetworkIssue()) return TaskOutcome.TEMPORARY_FAILURE;
+			if (te.exceededRateLimitation()) return TaskOutcome.TEMPORARY_FAILURE;
+			if (te.getErrorCode() == TWITTER_ERROR_CODE_STATUS_IS_A_DUPLICATE) return TaskOutcome.PREVIOUS_ATTEMPT_SUCCEEDED;
 			final int code = te.getStatusCode();
-			return code >= 400 && code < 500; // NOSONAR not magic numbers, this is HTTP spec.
+			return code >= 400 && code < 500 ? TaskOutcome.PERMANENT_FAILURE : TaskOutcome.TEMPORARY_FAILURE; // NOSONAR not magic numbers, this is HTTP spec.
 		}
 		else if (e instanceof SuccessWhaleException) {
-			return ((SuccessWhaleException) e).isPermanent();
+			return ((SuccessWhaleException) e).isPermanent() ? TaskOutcome.PERMANENT_FAILURE : TaskOutcome.TEMPORARY_FAILURE;
 		}
 		else if (e instanceof RuntimeException) {
-			return true;
+			return TaskOutcome.PERMANENT_FAILURE;
 		}
-		return false;
+		return TaskOutcome.TEMPORARY_FAILURE;
 	}
 
 }
