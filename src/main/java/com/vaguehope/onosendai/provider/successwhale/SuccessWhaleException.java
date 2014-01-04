a2105a0ab9918fe682bdbe22e49de010d58035de
はく <haku@vaguehope.com>
2014-01-04 13:42:34 +0000

    Make SW error msgs slight more friendly. Tidy exception reporting in FetchColumn.

--- src/main/java/com/vaguehope/onosendai/provider/successwhale/SuccessWhaleException.java
+++ src/main/java/com/vaguehope/onosendai/provider/successwhale/SuccessWhaleException.java
@@ -1,5 +1,11 @@
 package com.vaguehope.onosendai.provider.successwhale;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.vaguehope.onosendai.util.ExcpetionHelper;
+import com.vaguehope.onosendai.util.StringHelper;
+
 
 public class SuccessWhaleException extends Exception {
 
@@ -27,6 +33,22 @@ public class SuccessWhaleException extends Exception {
 
 	public boolean isPermanent () {
 		return this.permanent;
+	}
+
+	public String friendlyMessage () {
+		final Throwable cause = getCause();
+		if (cause != null) {
+			if (cause instanceof UnknownHostException) {
+				return "Network error: " + cause.getMessage();
+			}
+			else if (cause instanceof IOException && StringHelper.safeContainsIgnoreCase(cause.getMessage(), "connection timed out")) {
+				return "Network error: Connection timed out.";
+			}
+			else if (cause instanceof IOException) {
+				return "Network error: " + String.valueOf(cause);
+			}
+		}
+		return ExcpetionHelper.causeTrace(this);
 	}
 
 }
