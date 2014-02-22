0cfa2d4a67c4221c9722cfaa6a67b359d6e4e6c8
はく <haku@vaguehope.com>
2014-02-22 12:03:31 +0000

    Do not append slow loading tweet details under a different tweet. [issue #98]

--- src/main/java/com/vaguehope/onosendai/payload/ReplyLoaderTask.java
+++ src/main/java/com/vaguehope/onosendai/payload/ReplyLoaderTask.java
@@ -9,38 +9,37 @@ import com.vaguehope.onosendai.model.MetaType;
 import com.vaguehope.onosendai.model.Tweet;
 import com.vaguehope.onosendai.storage.DbInterface;
 import com.vaguehope.onosendai.util.DialogHelper;
+import com.vaguehope.onosendai.util.Result;
 import com.vaguehope.onosendai.util.exec.ExecutorEventListener;
 import com.vaguehope.onosendai.util.exec.TrackingAsyncTask;
-import com.vaguehope.onosendai.util.Result;
 
-public class ReplyLoaderTask extends TrackingAsyncTask<Tweet, Void, Result<List<Payload>>> {
+public class ReplyLoaderTask extends TrackingAsyncTask<Void, Void, Result<List<Payload>>> {
 
 	private final Context context;
 	private final DbInterface db;
+	private final Tweet tweet;
 	private final PayloadListAdapter payloadListAdaptor;
 
-	public ReplyLoaderTask (final ExecutorEventListener eventListener, final Context context, final DbInterface db, final PayloadListAdapter payloadListAdaptor) {
+	public ReplyLoaderTask (final ExecutorEventListener eventListener, final Context context, final DbInterface db, final Tweet tweet, final PayloadListAdapter payloadListAdaptor) {
 		super(eventListener);
 		this.context = context;
 		this.db = db;
 		this.payloadListAdaptor = payloadListAdaptor;
+		this.tweet = tweet;
 	}
 
 	@Override
 	public String toString () {
-		return "replyLoader"; // TODO include details.
+		return "replyLoader:" + this.tweet.getSid();
 	}
 
 	@Override
-	protected Result<List<Payload>> doInBackgroundWithTracking (final Tweet... params) {
+	protected Result<List<Payload>> doInBackgroundWithTracking (final Void... unused) {
 		try {
-			if (params.length != 1) throw new IllegalArgumentException("Only one param per task.");
-			final Tweet tweet = params[0];
-
-			final List<Tweet> replies = this.db.findTweetsWithMeta(MetaType.INREPLYTO, tweet.getSid(), 20);
+			final List<Tweet> replies = this.db.findTweetsWithMeta(MetaType.INREPLYTO, this.tweet.getSid(), 20);
 			final List<Payload> replyPayloads = new ArrayList<Payload>();
 			for (Tweet reply : replies) {
-				replyPayloads.add(new InReplyToPayload(tweet, reply));
+				replyPayloads.add(new InReplyToPayload(this.tweet, reply));
 			}
 			return new Result<List<Payload>>(replyPayloads);
 		}
@@ -52,7 +51,9 @@ public class ReplyLoaderTask extends TrackingAsyncTask<Tweet, Void, Result<List<
 	@Override
 	protected void onPostExecute (final Result<List<Payload>> result) {
 		if (result.isSuccess()) {
-			this.payloadListAdaptor.addItemsTop(result.getData());
+			if (this.payloadListAdaptor.isForTweet(this.tweet)) {
+				this.payloadListAdaptor.addItemsTop(result.getData());
+			}
 		}
 		else {
 			DialogHelper.alert(this.context, result.getE());
