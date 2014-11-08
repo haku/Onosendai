157f85ae82a5ef9c2aaeabaf93f3c723f6b0d919
haku <git@haku.me>
2014-11-08 19:36:50 +0000

    Force dereference bitmap in recycled row view before loading new one.

--- src/main/java/com/vaguehope/onosendai/model/TweetRowView.java
+++ src/main/java/com/vaguehope/onosendai/model/TweetRowView.java
@@ -28,6 +28,10 @@ class TweetRowView {
 		this.inlineMedia = inImageView;
 	}
 
+	public void reset () {
+		if (this.inlineMedia != null) this.inlineMedia.getImage().setImageDrawable(null);
+	}
+
 	public ImageView getAvatar () {
 		return this.avatar;
 	}
