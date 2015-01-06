haku/Onosendai

commit a4225f6de31bc9d4e16727a89f1b71e804544100

Stop ScrollIndicator unread marker ever regressing. [issue #144]

src/main/java/com/vaguehope/onosendai/widget/ScrollIndicator.java
@@ -148,6 +148,7 @@ public final class ScrollIndicator {
 		}
 
 		public void setUnreadTime (final long unreadTime) {
+			if (unreadTime > 0L && unreadTime <= this.unreadTime) return; // Can only go forwards.
 			this.unreadTime = unreadTime;
 			for (int i = 0; i < this.listAdaptor.getCount(); i++) {
 				if (this.listAdaptor.getItemTime(i) <= this.unreadTime) {

commit 13e1742dd72e1b24705bab42db3da1b7a7e1eb93

Fix unread marker not updating when column refreshed, broken in a4225f6. [issue #144]

src/main/java/com/vaguehope/onosendai/ui/TweetListFragment.java
@@ -353,7 +353,7 @@ public class TweetListFragment extends Fragment implements DbProvider {
 
 	protected void restoreJustUnreadFromDb () {
 		final ScrollState ss = getDb().getScroll(this.columnId);
-		this.scrollIndicator.setUnreadTime(ss.getUnreadTime());
+		this.scrollIndicator.setUnreadTimeIfNewer(ss.getUnreadTime());
 	}
 
 	protected void scrollTop () {

src/main/java/com/vaguehope/onosendai/widget/ScrollIndicator.java
@@ -44,6 +44,10 @@ public final class ScrollIndicator {
 		this.barMover.setUnreadTime(unreadTime);
 	}
 
+	public void setUnreadTimeIfNewer (final long unreadTime) {
+		if (unreadTime > this.barMover.getUnreadTime()) this.barMover.setUnreadTime(unreadTime);
+	}
+
 	public long getUnreadTime () {
 		return this.barMover.getUnreadTime();
 	}
@@ -148,7 +152,6 @@ public final class ScrollIndicator {
 		}
 
 		public void setUnreadTime (final long unreadTime) {
-			if (unreadTime > 0L && unreadTime <= this.unreadTime) return; // Can only go forwards.
 			this.unreadTime = unreadTime;
 			for (int i = 0; i < this.listAdaptor.getCount(); i++) {
 				if (this.listAdaptor.getItemTime(i) <= this.unreadTime) {